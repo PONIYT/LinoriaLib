db1a24db393d3104778175dbe5bc297685a5a6a3
PONIYT <121174065+PONIYT@users.noreply.github.com>
2025-02-13 18:45:22 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -113,12 +113,12 @@ local Library = {
 
 	HudRegistry = {},
 
-	FontColor = Color3.fromRGB(255, 255, 255),
-	MainColor = Color3.fromRGB(28, 28, 28),
-	BackgroundColor = Color3.fromRGB(20, 20, 20),
-	AccentColor = Color3.fromRGB(0, 85, 255),
-	OutlineColor = Color3.fromRGB(50, 50, 50),
-	RiskColor = Color3.fromRGB(255, 50, 50),
+	FontColor = Color3.fromRGB(0, 255, 0),
+        MainColor = Color3.fromRGB(10, 10, 10),
+        BackgroundColor = Color3.fromRGB(5, 5, 5),  
+        AccentColor = Color3.fromRGB(0, 200, 0),  
+        OutlineColor = Color3.fromRGB(20, 20, 20),
+        RiskColor = Color3.fromRGB(255, 0, 0),
 
 
 	Black = Color3.new(0, 0, 0),
